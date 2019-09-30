7ee9366c47de2cc85325ce58f2a3515cb7aa32ba
Rakesh Kumar <rkrakesh133@gmail.com>
2019-09-30 15:25:56 +0530

    Add files via upload

---
+++ search.py
@@ -0,0 +1,19 @@
+from colorama import Fore,Back,Style,init
+from termcolor import colored
+init()
+f = open('myfile.txt','r')
+l = f.read().split(' ')
+for word in l:
+	print(word,end =' ')
+print()
+print()
+s = input('Enter sub string: ')
+print()
+print()
+print(colored("Words having '"+s+"' as substring",'green','on_red'))
+for word in l:
+	pos = word.find(s)
+	if(pos != -1):
+		print(colored(word,'white','on_red'),end=' ')
+	else:
+		print(word,end =' ')
